051160e13ad9cda2f2cf4862d98a79dcd40afabd
Yohan Boniface <yb@enix.org>
2020-12-01 09:23:14 +0100

    fix(bin): add déclarant key to explore command

--- egapro/bin.py
+++ egapro/bin.py
@@ -352,6 +352,7 @@ async def explore(*siren_year):
             "indicateurs.rémunérations",
             "entreprise",
             "déclaration",
+            "déclarant",
         ]:
             print(sep)
             print(f"# {root}")
