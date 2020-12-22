8ed771e8c89efde1a3726cabebf2ca2a8cea3ef0
Yohan Boniface <yb@enix.org>
2020-12-22 15:21:54 +0100

    fix(migration): reset idx_effectif index on migration to new schema

--- egapro/sql/create_indexes.sql
+++ egapro/sql/create_indexes.sql
@@ -1,3 +1,3 @@
-CREATE INDEX IF NOT EXISTS idx_effectifs ON declaration ((data->'informations'->'trancheEffectifs'));
+CREATE INDEX IF NOT EXISTS idx_effectifs ON declaration ((data->'entreprise'->'effectifs'->'tranche'));
 CREATE INDEX IF NOT EXISTS idx_ft ON declaration USING GIN (ft);
 CREATE INDEX IF NOT EXISTS idx_status ON declaration (declared_at) WHERE declared_at IS NOT NULL;
