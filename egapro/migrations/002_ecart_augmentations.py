eab369d1ea80c069d7188a9cdd66e01f8577f81c
Yohan Boniface <yb@enix.org>
2021-01-26 19:32:40 +0100

    feat(migrations): add minimal migrations system and and 4 data migrations

---
+++ egapro/migrations/002_ecart_augmentations.py
@@ -0,0 +1,45 @@
+"""Data migration from 2020-12-25 did not compute missing ecartTauxAugmentation."""
+
+import progressist
+
+
+async def main(db, logger):
+
+    records = await db.declaration.fetch(
+        "SELECT * FROM declaration WHERE data->>'source'='simulateur' "
+        "AND legacy IS NOT NULL"
+    )
+    bar = progressist.ProgressBar(prefix="Migrating…", total=len(records))
+    for record in bar.iter(records):
+        if not record.data.path("indicateurs.augmentations"):
+            continue
+        if record.data.path("indicateurs.augmentations.non_calculable"):
+            continue
+        data = record.data.raw
+        siren = record.data.siren
+        year = record.data.year
+        try:
+            deux = record["legacy"]["indicateurDeux"]
+        except KeyError:
+            continue
+        tranches = deux.get("tauxAugmentation")
+        if tranches:
+            categories = []
+            for tranche in tranches:
+                if "ecartTauxAugmentation" in tranche:
+                    categories.append(tranche["ecartTauxAugmentation"])
+                elif (h := tranche.get("tauxAugmentationHommes")) and (
+                    f := tranche.get("tauxAugmentationFemmes")
+                ):
+                    categories.append(1 - f / h)
+                else:
+                    categories.append(None)
+        if any(c is not None for c in categories):
+            data["indicateurs"]["augmentations"]["catégories"] = categories
+            await db.declaration.put(
+                siren,
+                year,
+                owner=record["owner"],
+                data=data,
+                modified_at=record["modified_at"],
+            )
