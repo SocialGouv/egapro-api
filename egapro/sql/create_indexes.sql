393b37d60c308fdaca8732d55eb05b65db9f2603
Yohan Boniface <yb@enix.org>
2021-02-02 12:01:29 +0100

    feat(search): more indexes

--- egapro/sql/create_indexes.sql
+++ egapro/sql/create_indexes.sql
@@ -1,4 +1,5 @@
 CREATE INDEX IF NOT EXISTS idx_effectifs ON declaration ((data->'entreprise'->'effectifs'->'tranche'));
 CREATE INDEX IF NOT EXISTS idx_status ON declaration (declared_at) WHERE declared_at IS NOT NULL;
 CREATE INDEX IF NOT EXISTS idx_ft ON search USING GIN (ft);
-CREATE INDEX IF NOT EXISTS idx_ft ON search USING GIN (departement);
+CREATE INDEX IF NOT EXISTS idx_departement ON search(departement);
+CREATE INDEX IF NOT EXISTS idx_naf ON search(code_naf);
