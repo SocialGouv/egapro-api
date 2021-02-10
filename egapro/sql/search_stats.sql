a4079cd578d4eea213868a520b9d2e2b2f44c15f
Yohan Boniface <yohanboniface@free.fr>
2021-02-10 12:12:34 +0100

    fix(search): naive way to compute count on real results

--- egapro/sql/search_stats.sql
+++ egapro/sql/search_stats.sql
@@ -1,4 +1,8 @@
-SELECT COUNT(*), avg((data->'déclaration'->>'index')::int), min((data->'déclaration'->>'index')::int), max((data->'déclaration'->>'index')::int) FROM declaration WHERE year=$1 AND siren IN (
-    SELECT siren FROM search
-    {where}
-)
+WITH search AS (SELECT siren FROM search {where}),
+     count AS (SELECT COUNT(DISTINCT(siren)) FROM search),
+     stats AS (SELECT avg((data->'déclaration'->>'index')::int),
+                      min((data->'déclaration'->>'index')::int),
+                      max((data->'déclaration'->>'index')::int)
+               FROM declaration d JOIN search s ON d.siren=s.siren
+               WHERE d.year=$1)
+    SELECT * FROM count JOIN stats ON true;
