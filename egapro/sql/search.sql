6373e1e88f4bf670c4e6063a9b47d24b6ddc366a
Yohan Boniface <yohanboniface@free.fr>
2021-03-02 10:06:06 +0100

    fix(search): better way to combine group by and order by / limit

--- egapro/sql/search.sql
+++ egapro/sql/search.sql
@@ -1,7 +1,8 @@
 SELECT array_agg(data ORDER BY declared_at DESC) as data, jsonb_object_agg(year::text, (data->'déclaration'->>'index')::int) as notes FROM declaration WHERE siren in (
     SELECT siren FROM search
     {where}
-    ORDER BY declared_at DESC
+    GROUP BY siren
+    ORDER BY max(declared_at) DESC
     LIMIT $1
     OFFSET $2
 )
