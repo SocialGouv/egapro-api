674a4a300008d04e30ecdc95ba874356beb786ea
Yohan Boniface <yohanboniface@free.fr>
2021-03-08 13:36:20 +0100

    fix(db): Replace JOIN by subquery on search stats
    That query was very slow on prod (while being fast on local computed
    with same dataset…)

--- egapro/sql/search_stats.sql
+++ egapro/sql/search_stats.sql
@@ -1,8 +1,8 @@
-WITH search AS (SELECT siren FROM search {where}),
-     count AS (SELECT COUNT(DISTINCT(siren)) FROM search),
+WITH subset AS (SELECT siren FROM search {where}),
+     count AS (SELECT COUNT(DISTINCT(siren)) FROM subset),
      stats AS (SELECT avg((data->'déclaration'->>'index')::int),
                       min((data->'déclaration'->>'index')::int),
                       max((data->'déclaration'->>'index')::int)
-               FROM declaration d JOIN search s ON d.siren=s.siren
-               WHERE d.year=$1)
+               FROM declaration
+               WHERE year=$1 AND siren IN (SELECT siren FROM subset))
     SELECT * FROM count JOIN stats ON true;
