6c1513d2a59f343e50df71f186c309b4220c7b2b
Ali <alitoch@gmail.com>
2022-07-20 18:08:57 +0200

    Update email body (#88)
    * Update email body
    
    * updating body in html and text + added date_publication condition
    
    * separating objective email from declaration email
    
    * adding new endpoint from objectifs receipt
    
    * adding subject to objectifs email
    
    * reverting changes in success emails (now that we have objectives emails)

--- egapro/views.py
+++ egapro/views.py
@@ -201,6 +201,22 @@ async def resend_receipt(request, response, siren, year):
     response.status = 204
 
 
+@app.route("/declaration/{siren}/{year}/objectifs-receipt", methods=["POST"])
+@tokens.require
+@ensure_owner
+async def resend_objectifs_receipt(request, response, siren, year):
+    try:
+        record = await db.declaration.get(siren, year)
+    except db.NoData:
+        raise HttpError(404, f"No declaration with siren {siren} and year {year}")
+    owners = await db.ownership.emails(siren)
+    if not owners:  # Staff member
+        owners = request["email"]
+    data = record.data
+    emails.objectives.send(owners, **data)
+    response.status = 204
+
+
 @app.route("/ownership/{siren}", methods=["GET"])
 @tokens.require
 @ensure_owner
